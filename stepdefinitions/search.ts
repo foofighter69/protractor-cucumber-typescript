b4538fd481a8ba18630456efc79481f560eff183
biroz <biroz@astron.hu>
2019-07-02 23:09:30 +0200

    element not interactable fix

--- stepdefinitions/search.ts
+++ stepdefinitions/search.ts
@@ -13,6 +13,8 @@ When(/^I click on search button$/, async () => {
 });
 
 When(/^I hit enter in the search input$/, async () => {
+	var until = protractor.ExpectedConditions;
+	browser.wait(until.presenceOf(search.searchTextBox), 5000, 'Element taking too long to appear in the DOM');
     await search.searchTextBox.sendKeys(protractor.Key.ENTER);
 });
 
